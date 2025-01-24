e3e9dd27950c4d743f48e8b3b0d9d1e2bac1f146
rozennrd <r_renaud@gmx.fr>
2025-01-24 09:31:18 +0100

    Tp1 exercice 1 : Hello World !

---
+++ Resources/main.c
@@ -0,0 +1,11 @@
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+void main(void) {
+    /* Code d'initialisation */
+
+    while(1){
+        /* Code a executer dans une boucle infinie */
+    }
+}
+
---
+++ TP_Microcontroleurs/TP1a_helloworld.X/main.c
@@ -0,0 +1,39 @@
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+// Configurer la fréquence d'horloge: 8MHz
+#define _XTAL_FREQ 8000000
+
+// Attendre 500ms
+ void delai_approx(void) {
+        __delay_ms(500);
+    }
+
+void main(void) {
+    /* Code d'initialisation */
+
+     // Configurer les broches des registres B et D (contrôlant les LEDs) comme sorties
+    TRISD = 0x00; 
+    TRISB = 0x00;
+   
+    
+    while (1) { // Toujours
+        /* Code à exécuter dans une boucle infinie */
+        // Allumer les LEDs  5 à 8 (registre B), éteindre les leds 1 à 4 (registre D)
+        LATD = 0x00;
+        LATB = 0x0F;
+        
+        // Attendre 500ms
+        delai_approx();
+        
+        // Éteindre les LEDs  5 à 8 (registre B), allumer les leds 1 à 4 (registre D)
+        LATB = 0x00;
+        LATD = 0x0F; 
+        // Attendre 500 ms. Le temps total approximatif de la boucle est 1s. 
+        delai_approx();
+    }
+    
+   
+    
+}
+
